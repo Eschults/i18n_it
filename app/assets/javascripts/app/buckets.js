44d39e4f53aff5d5c2fa090393a8b7530d594f1c
Edward Schults <edward.schults@gmail.com>
2015-08-21 17:52:27 +0200

    buckets#update works with multiple sub_buckets

---
+++ app/assets/javascripts/app/buckets.js
@@ -0,0 +1,14 @@
+$('.sub-bucket-nav-link').on('click', function() {
+  $('.sub-bucket-active').removeClass('sub-bucket-active')
+  $(this).addClass('sub-bucket-active')
+  var id = $(this).attr('id')
+  $('.edit-sub-bucket').hide();
+  $('#sub_bucket_' + id).show();
+})
+
+$('input[type="submit"').on('click', function(event) {
+  event.preventDefault()
+  var id = $(this).attr('id').slice($(this).attr('id').lastIndexOf('_') + 1, $(this).attr('id').length)
+  $('#sub_bucket_id').val(id)
+  $('form').submit()
+})
--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -39,15 +39,17 @@ class BucketsController < ApplicationController
       @bucket.project.languages.each do |language|
         if @bucket.kind == "s"
           translation = Translation.find_or_create_by(translation_key: key, language: language, bucket: @bucket) unless params[:translation_keys][:value] == ""
-        elsif @bucket.kind == "d"
-          translation = Translation.find_or_create_by(translation_key: key, language: language, bucket: @bucket, sub_bucket: @sub_bucket)
-          key = key.split("_").map { |word| word.capitalize }.join(' ')
-        end
-        unless translation.nil?
           translation.translation_key = params[:translation_keys][key][:value] unless translation.translation_key == params[:translation_keys][key][:value]
           translation.text = params[:translation_keys][key][language.language_key] unless translation.translation_key == params[:translation_keys][key][language.language_key]
+        elsif @bucket.kind == "d"
+          camelKey = key.split("_").map { |word| word.capitalize }.join(' ')
+          translation = Translation.find_or_create_by(translation_key: key, language: language, bucket: @bucket, sub_bucket: @sub_bucket)
+          translation.translation_key = params[:translation_keys]['sub_bucket_' + @sub_bucket.id.to_s][camelKey][:value] unless translation.translation_key == params[:translation_keys]['sub_bucket_' + @sub_bucket.id.to_s][camelKey][:value]
+          translation.text = params[:translation_keys]['sub_bucket_' + @sub_bucket.id.to_s][camelKey][language.language_key] unless translation.translation_key == params[:translation_keys]['sub_bucket_' + @sub_bucket.id.to_s][camelKey][language.language_key]
+          translation.sub_bucket = @sub_bucket
+        end
+        unless translation.nil?
           translation.bucket = @bucket
-          translation.sub_bucket = @sub_bucket if @bucket.kind == "d"
           translation.language = language
           translation.save
         end
@@ -62,14 +64,13 @@ class BucketsController < ApplicationController
     elsif @bucket.kind == "d"
       @bucket.bucket_schemas.each do |bucket_schema|
         @bucket.project.languages.each do |language|
-          unless Translation.find_by(language: language, bucket: @bucket, sub_bucket: @sub_bucket, translation_key: params[:translation_keys][bucket_schema.bucket_schema_name][:value], text: params[:translation_keys][bucket_schema.bucket_schema_name][language.language_key])
-            Translation.create(language: language, bucket: @bucket, sub_bucket: @sub_bucket, translation_key: params[:translation_keys][bucket_schema.bucket_schema_name][:value], text: params[:translation_keys][bucket_schema.bucket_schema_name][language.language_key]) unless params[:translation_keys][bucket_schema.bucket_schema_name][language.language_key] == ""
+          unless Translation.find_by(language: language, bucket: @bucket, sub_bucket: @sub_bucket, translation_key: params[:translation_keys]['sub_bucket_' + @sub_bucket.id.to_s][bucket_schema.bucket_schema_name][:value], text: params[:translation_keys]['sub_bucket_' + @sub_bucket.id.to_s][bucket_schema.bucket_schema_name][language.language_key])
+            Translation.create(language: language, bucket: @bucket, sub_bucket: @sub_bucket, translation_key: params[:translation_keys]['sub_bucket_' + @sub_bucket.id.to_s][bucket_schema.bucket_schema_name][:value], text: params[:translation_keys]['sub_bucket_' + @sub_bucket.id.to_s][bucket_schema.bucket_schema_name][language.language_key]) unless params[:translation_keys]['sub_bucket_' + @sub_bucket.id.to_s][bucket_schema.bucket_schema_name][language.language_key] == ""
           end
         end
       end
-
     end
-    redirect_to edit_bucket_path(@bucket)
+    redirect_to edit_bucket_path(@bucket, anchor: @sub_bucket.id)
   end
 
   private
